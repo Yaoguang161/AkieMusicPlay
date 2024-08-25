c5ee4b6f868b05d2f03238495f1d32a5662571f9
yaoguang161 <yaoguang161@outlook.com>
2024-08-25 20:26:55 +0800

    新增grpc和Qt环境配置

--- sample/main.cpp
+++ sample/main.cpp
@@ -2,11 +2,37 @@
 
 #include <iostream>
 #include <spdlog/spdlog.h>
-#include <grpc++/grpc++.h>
 #include "test/hello.h"
+#include <QApplication>
+#include <QWidget>
+#include <grpc++/grpc++.h> 
+#include <grpcpp/grpcpp.h>
 
-int main()
+class FirstWindow : public QWidget {
+public:
+    FirstWindow() {
+        setWindowTitle("First Window");
+        resize(400, 300);  // 设置窗口大小
+    }
+};
+
+int main(int argc, char *argv[])
 {
+    QApplication app(argc, argv);
+
+    // 创建第一个窗口
+    FirstWindow window1;
+    window1.show();
+
+    // 创建一个 gRPC 通道，指向一个假设的服务器地址
+    auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
+
+    if (channel) {
+        std::cout << "gRPC channel created successfully!" << std::endl;
+    } else {
+        std::cout << "Failed to create gRPC channel." << std::endl;
+    }
+
     spdlog::warn("Ola povo!");
     spdlog::info("Hello world!");
     std::cout  << "Hello CMake!" << std::endl;
